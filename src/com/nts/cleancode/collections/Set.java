8a109b433a6950da3bb891b740c02e5c85a29aba
Insung Kim <isk1149@daum.net>
2016-06-02 16:38:18 +0900

    expression modify & extract method

--- src/com/nts/cleancode/collections/Set.java
+++ src/com/nts/cleancode/collections/Set.java
@@ -19,20 +19,35 @@ public class Set extends AbstractCollection {
 	}
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + INITIAL_CAPACITY];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
-
-			if (contains(element))
-				return;
-			elements[size++] = element;
+		if (readOnly) 
+			return;
+			
+		if (shouldGrow()) {
+			grow();
 		}
+
+		if (contains(element))
+			return;
+			
+		addElement(element);
+		
+	}
+
+	private void addElement(Object element) {
+		elements[size++] = element;
+	}
+
+	private boolean shouldGrow() {
+		return size + 1 > elements.length;
+	}
+
+	private void grow() {
+		Object[] newElements =
+			new Object[elements.length + INITIAL_CAPACITY];
+		
+		for (int i = 0; i < size; i++)
+			newElements[i] = elements[i];
+		elements = newElements;
 	}
 
 	public boolean contains(Object element) {
